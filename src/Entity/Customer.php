5892408f14081b945ec89c14848c0c96f77e3cb6
jeremy bouvier <jeremybouvier2b@gmail.com>
2019-09-21 13:44:13 +0200

    Configuring Resource Filtering and Pagination

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -3,13 +3,29 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ApiResource(
  *     collectionOperations={"get","post"},
- *     itemOperations={"get","put","delete"}
+ *     itemOperations={"get","delete"},
+ *     attributes={"pagination_client_items_per_page"=true, "maximum_items_per_page"=20}
  * )
+ * @ApiFilter(OrderFilter::class, properties={"lastName":"ASC"})
+ * @ApiFilter(
+ *     SearchFilter::class,
+ *     properties={
+ *          "firstName":"iexact",
+ *          "lastName":"iexact",
+ *          "address":"ipartial",
+ *          "email":"iexact",
+ *          "city":"iexact",
+ *          "zipCode":"exact"
+ *      }
+ *  )
  * @ORM\Entity(repositoryClass="App\Repository\CustomerRepository")
  */
 class Customer
@@ -39,7 +55,7 @@ class Customer
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $adress;
+    private $address;
 
     /**
      * @ORM\Column(type="string", length=255)
@@ -97,14 +113,14 @@ class Customer
         return $this;
     }
 
-    public function getAdress(): ?string
+    public function getAddress(): ?string
     {
-        return $this->adress;
+        return $this->address;
     }
 
-    public function setAdress(string $adress): self
+    public function setAddress(string $address): self
     {
-        $this->adress = $adress;
+        $this->address = $address;
 
         return $this;
     }
@@ -133,12 +149,12 @@ class Customer
         return $this;
     }
 
-    public function getUser(): ?User
+    public function getUser()
     {
         return $this->user;
     }
 
-    public function setUser(?User $user): self
+    public function setUser( $user): self
     {
         $this->user = $user;
 
