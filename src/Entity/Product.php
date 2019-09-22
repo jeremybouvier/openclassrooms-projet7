68ac8d44c16f71de8de50f0e87ba7e235b21325c
jeremy bouvier <jeremybouvier2b@gmail.com>
2019-09-22 14:07:52 +0200

    Configuring Resource Filtering and Pagination

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -8,7 +8,6 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
